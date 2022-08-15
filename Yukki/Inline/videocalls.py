4cc29d057a8a6e337adb809a149ec7577fc51bd5
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-08-15 09:29:52 +0700

    Update videocalls.py

--- Yukki/Inline/videocalls.py
+++ Yukki/Inline/videocalls.py
@@ -16,7 +16,7 @@ def choose_markup(videoid, duration, user_id):
         ],
         [
             InlineKeyboardButton(
-                text="ᴄʟᴏsᴇ",
+                text="ᴛᴜᴛᴜᴘ",
                 callback_data=f"forceclose {videoid}|{user_id}",
             )
         ],
@@ -32,7 +32,7 @@ def livestream_markup(quality, videoid, duration, user_id):
                 callback_data=f"LiveStream {quality}|{videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="ᴄʟᴏsᴇ",
+                text="ᴛᴜᴛᴜᴘ",
                 callback_data=f"forceclose {videoid}|{user_id}",
             ),
         ],
@@ -58,11 +58,11 @@ def stream_quality_markup(videoid, duration, user_id):
         ],
         [
             InlineKeyboardButton(
-                text="ɢᴏ ʙᴀᴄᴋ",
+                text="ᴋᴇᴍʙᴀʟɪ",
                 callback_data=f"gback_list_chose_stream {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="ᴄʟᴏsᴇ",
+                text="ᴛᴜᴛᴜᴘ",
                 callback_data=f"forceclose {videoid}|{user_id}",
             ),
         ],
